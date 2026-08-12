d1424981c3f3551f47702025c44a4f3eeff370ce
vincentnguyen <krd2et@virginia.edu>
2026-08-12 00:38:55 -0400

    Initial Commit

---
+++ NotesPro/NotesProApp.swift
@@ -0,0 +1,32 @@
+//
+//  NotesProApp.swift
+//  NotesPro
+//
+//  Created by Vincent Nguyen on 8/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NotesProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
